b340bae24410da07489373b71eccf39e3e01392a
David Sanson <dsanson@gmail.com>
2015-03-20 12:50:26 -0500

    putting quotes around $bibfile
    Maybe this will fix issue #1

--- exportMarkdownReferences.sh
+++ exportMarkdownReferences.sh
@@ -11,7 +11,7 @@ read refs
 
 command -v pandoc >/dev/null 2>&1 || { echo "Cannot find pandoc in path. Edit the PATH variable in citeServiceTemplate-pandoc-bib-script.sh"; exit 0;}
 
-echo "$refs" | pandoc --bibliography $bibfile -t markdown --no-wrap 2> /dev/null | tail -n +3
+echo "$refs" | pandoc --bibliography "$bibfile" -t markdown --no-wrap 2> /dev/null | tail -n +3
 
 exit 0
 
